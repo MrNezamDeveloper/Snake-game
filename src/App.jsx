c192ca90f3a5ff2a2f53a5f6c8300f337b52d7d9
Mohamad Nezam <moonezam@gmail.com>
2021-05-25 16:40:27 +0430

    change map and come bck to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,50 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import './App.css'
 import Items from "./componenets/Items";
 
 
-const App = () =>{
+
+export default class App extends Component{
 // create random food for snake
-
- const [FoodR, setFoodR] = useState(Math.floor(Math.random()*20))
- const [FoodC, setFoodC] = useState(Math.floor(Math.random()*20))
-
-  const box = () =>{
-   let arry = []
-   // create table 20*20 row and colemn
-    for (let i = 0; i < 20; i++) {
-      for (let j = 0; j < 20; j++) {
-        // create food as random method
-        if (i === FoodR && j === FoodC) {
-          // send props to items as class name 
-          arry.push(<Items className="food"/>)
-        }else{
-          arry.push(<Items className="item"/>)
-        }
-        
-      }
-    }
-
-   return arry
+constructor(props){
+  super(props)
+ this.state = {
+  food : this.getRandomCell()
   }
-  
-  return(
-    <div className="box">
-     {box()}
-    </div>
-  )
-  
 }
-export default App;
+
+getRandomCell = () => {
+  return{
+    column : Math.floor(Math.random() *20),
+    row : Math.floor(Math.random() *20),
+  }
+}
+
+
+
+box = () =>{
+  let arry = []
+  // create table 20*20 row and colemn
+   for (let i = 0; i < 20; i++) {
+     for (let j = 0; j < 20; j++) {
+       // create food as random method
+       if (i === this.state.food.row && j === this.state.food.column) {
+         // send props to items as class name 
+         arry.push(<Items className="food"/>)
+       }else{
+         arry.push(<Items className="item"/>)
+       }
+     }
+   }
+
+  return arry
+ }
+render(){
+ 
+return(
+  <div className="box">
+    {this.box()}
+  </div>
+  )
+}
+}
